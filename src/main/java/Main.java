caa91762c1732cd3c118166fca8b7f0d41019dfc
Marwane <marwane.benou-kaiss@etu.u-bordeaux.fr>
2018-03-30 09:50:15 +0200

    Chargement de l'UI dans Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Created on 30/03/18.
+ *
+ * @author mbenoukaiss
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        ResourceBundle resources = ResourceBundle.getBundle("bundles/game", Locale.ENGLISH);
+        Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"), resources);
+
+        Scene scene = new Scene(root, 900, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
